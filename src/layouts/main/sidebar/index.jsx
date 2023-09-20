9070a2e1a9023be927065b143afde5aef5f0139e
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-09-20 16:24:04 +0300

    added with anasayfa and kesfet icons

--- src/layouts/main/sidebar/index.jsx
+++ src/layouts/main/sidebar/index.jsx
@@ -1,7 +1,11 @@
+import Logo from "./logo";
+import Menu from "./menu";
+
 export default function Siderbar() {
     return (
         <aside className="w-[275px] min-h-screen">
-            sidebar
+            <Logo />
+            <Menu />
         </aside>
     )
 }
--- src/layouts/main/sidebar/logo/index.jsx
+++ src/layouts/main/sidebar/logo/index.jsx
@@ -13,4 +13,4 @@ export default function Logo() {
 			</Link>
 		</div>
 	)
-}
+} 
