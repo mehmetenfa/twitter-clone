0f67449f7a9e0fecd55bac3b3a4eab97bfa64834
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-12-12 17:50:56 +0300

    rightbar

--- src/layouts/main/rightbar/index.jsx
+++ src/layouts/main/rightbar/index.jsx
@@ -1,12 +1,13 @@
-import Search from "~/layouts/main/rightbar/search/index.jsx";
-import Premium from "~/components/premium/index.jsx";
+import Search from "~/layouts/main/rightbar/search";
+import Premium from "~/layouts/main/rightbar/premium";
+import Topics from "~/layouts/main/rightbar/topics";
 
 export default function RightBar() {
-    return (
-        <aside className="w-[350px] mr-2.5">
-            <Search />
-            <Premium />
-        </aside>
-    )
+   return (
+       <aside className="w-[350px] mr-2.5">
+          <Search />
+          <Premium />
+          <Topics />
+       </aside>
+   )
 }
-
